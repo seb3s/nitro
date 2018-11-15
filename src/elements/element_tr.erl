cacd3b50a8aef68bc19fd85cd7cda924082a7fb4
seb3s <seb@matchix.com>
2018-11-15 16:32:49 +0100

    Cursor should not be hardcoded at this level. Let's factorize like the other elements

--- src/elements/element_tr.erl
+++ src/elements/element_tr.erl
@@ -2,29 +2,9 @@
 -include_lib("nitro/include/nitro.hrl").
 -compile(export_all).
 
-render_element(Record = #tr{postback= Postback}) ->
-    Id = case Record#tr.id of undefined -> nitro:temp_id(); I->I end,
-    Cursor = case Postback of 
-        undefined -> "";
-        P -> nitro:wire(#event {type=click, postback=P, target=Id, delegate=Record#tr.delegate}),
-            "cursor:pointer;"
-    end,
+render_element(Record) ->
+    Id = nitro:wire_postback(Record),
     wf_tags:emit_tag(<<"tr">>, nitro:render(Record#tr.cells), [
-        {<<"id">>,              Id},
-        {<<"class">>,           element(#element.class, Record)},
-        {<<"style">>,           [element(#element.style, Record), Cursor]}, %% CAUTION
-        {<<"title">>,           element(#element.title, Record)},
-        {<<"accesskey">>,       element(#element.accesskey, Record)},
-        {<<"contenteditable">>, case element(#element.contenteditable, Record) of true -> "true"; false -> "false"; _ -> undefined end},
-        {<<"contextmenu">>,     element(#element.contextmenu, Record)},
-        {<<"dir">>,             case element(#element.dir, Record) of "ltr" -> "ltr"; "rtl" -> "rtl"; "auto" -> "auto"; _ -> undefined end},
-        {<<"draggable">>,       case element(#element.draggable, Record) of true -> "true"; false -> "false"; _ -> undefined end},
-        {<<"dropzone">>,        element(#element.dropzone, Record)},
-        {<<"hidden">>,          case element(#element.hidden, Record) of "hidden" -> "hidden"; _ -> undefined end},
-        {<<"lang">>,            element(#element.lang, Record)},
-        {<<"spellcheck">>,      case element(#element.spellcheck, Record) of true -> "true"; false -> "false"; _ -> undefined end},
-        {<<"tabindex">>,        element(#element.tabindex, Record)},
-        {<<"translate">>,       case element(#element.translate, Record) of "yes" -> "yes"; "no" -> "no"; _ -> undefined end},
-        {<<"role">>,            element(#element.role, Record)}
+        ?NITRO_GLOBAL_ATTRIBUTES(Id)
         ?NITRO_DATA_ARIA_ATTRIBUTES
     ]).
